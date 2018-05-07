3f025a1059a389e4ef1b14ffc86f4f9775217113
Pablo Planelló <36472624+pPlanello@users.noreply.github.com>
2018-05-07 21:31:08 +0200

    Version1

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+const sequelize = require('../models/index.js');
+//const Sequelize = require('sequelize');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/credits', function(req, res, next) {
+  res.render('credits', { title: 'PABLO' });
+});
+
+router.get('/quizzes', function(req, res, next) {
+  sequelize.models.quiz.findAll()
+	.then(quiz => {
+		res.render ('quizzes', {array: quiz});
+
+	})
+	.catch(error => next(error));
+});
+
+module.exports = router;
